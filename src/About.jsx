151799ffbce43dc3ab3dc1f93018f235967e9b70
akash-akku <142794376+akash-akku@users.noreply.github.com>
2023-08-21 15:52:58 +0530

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Leftsidebar from './component/sidebar/Leftsidebar'
+import Rightsidebar from './component/sidebar/Rightsidebar'
+import Homemainbar from './component/sidebar/Homemainbar'
+export default function About() {
+  return (
+    <div className='home-container-1'>
+    <Leftsidebar/>
+    <div className='home-container-2'>
+    <Homemainbar/>
+    <Rightsidebar />
+    </div>
+    </div>
+  )
+}
